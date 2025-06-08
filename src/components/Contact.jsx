1f334259c1c35f5803f8945e4de24f9a3a21b360
Simeon John <115508839+SimeonJay@users.noreply.github.com>
2025-06-08 13:12:55 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,33 @@
 import React, { useRef } from "react";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
 import Reveal from "./Reveal";
+import Swal from 'sweetalert2'
 
-const Contact = () => {
-  const form = useRef();
+export default function Contact() {
+  const [result, setResult] = React.useState("");
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
 
-    emailjs
-      .sendForm("service_dl18mvb", "template_4tfef5a", form.current, {
-        publicKey: "W865RXXtAMRDPzdTo",
-      })
-      .then(
-        () => {
-          console.log("SUCCESS!");
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
+    formData.append("access_key", "0cc982b5-42ba-458b-bbe1-264c317ef36d");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      Swal.fire({
+        title: "Success",
+        text: "Message sent successfully!",
+        icon: "success"
+      });
+    }
   };
 
   return (
@@ -64,22 +70,10 @@ const Contact = () => {
                   <span>years of experience</span>
                 </p>
               </div>
-
-              {/* <div className="bg-gray-800/40 p-5 rounded-lg">
-                <h3 className="md:text-4xl text-2xl font-semibold text-white">
-                  30
-                  <span>+</span>
-                </h3>
-                <p className="text-xs md:text-base">
-                  <span>happy clients</span>
-                </p>
-              </div> */}
             </div>
           </div>
 
-          <form
-            ref={form}
-            onSubmit={sendEmail}
+          <form onSubmit={onSubmit}
             className=" max-w-6xl p-5 md:p-12"
           >
             <p className="text-gray-100 font-bold text-xl mb-2">
@@ -89,16 +83,16 @@ const Contact = () => {
               type="text"
               id="name"
               placeholder="Your Name ..."
-              name="from_name"
+              name="name"
               className="mb-2 w-full rounded-md border border-purple-600 py-2 pl-2 pr-4"
-            />
+            required/>
             <input
               type="email"
               id="email"
               placeholder="Your Email ..."
-              name="from_email"
+              name="email"
               className="mb-2 w-full rounded-md border border-purple-600 py-2 pl-2 pr-4"
-            />
+              required/>
             <textarea
               name="message"
               id="textarea"
@@ -106,7 +100,7 @@ const Contact = () => {
               rows="4"
               placeholder="Your Message ..."
               className="mb-2 w-full rounded-md border border-purple-600 py-2 pl-2 pr-4"
-            />
+              required/>
             <button
               type="submit"
               className="w-full py-3 rounded-md text-gray-100 font-semibold text-xl bg-primary-color"
@@ -120,5 +114,3 @@ const Contact = () => {
     </div>
   );
 };
-
-export default Contact;
